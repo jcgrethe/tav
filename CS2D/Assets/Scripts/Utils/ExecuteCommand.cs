8c8a1ca0d79ee9f9bcb0edd8e2009f84209e9671
jcgrethe <jgrethe@itba.edu.ar>
2020-10-23 21:01:32 -0300

    differents speed when shooting or crouch

--- CS2D/Assets/Scripts/Utils/ExecuteCommand.cs
+++ CS2D/Assets/Scripts/Utils/ExecuteCommand.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static AnimatorStates;
 
 public class ExecuteCommand
 {
@@ -8,10 +9,11 @@ public class ExecuteCommand
 
     public static void Execute(Command command, GameObject client, CharacterController characterController)
     {
-        
         float gravity = 18000f;
         float jumpSpeed = 3000;
         float verticalSpeed = 0;
+        float speed = 300;
+
         float horizontalMove = command.HorizontalMove;
         float verticalMove = command.VerticalMove;
         if (characterController.isGrounded  && command.Jump)
@@ -19,9 +21,17 @@ public class ExecuteCommand
             verticalSpeed = jumpSpeed;
         }
         verticalSpeed -= gravity * Time.deltaTime;
+        if (IsCrouch(command))
+        {
+            speed = speed / 2;
+        }
+        else if (IsShooting(command))
+        {
+            speed = speed / 1.5f;   
+        }
         Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
         Vector3 move = client.transform.forward * verticalMove + client.transform.right * horizontalMove;
-        characterController.Move(300 * Time.deltaTime * move + gravityMove * Time.deltaTime);
+        characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
         Rotate(client, command);
     }
     
